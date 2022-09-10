d8d82602cab591f07ff9db96cb677c85f411aa12
rainbowflesh <rainbow@qiuqi-tech.com>
2022-09-11 02:01:55 +0800

    chore

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/zsh
 
 # abort on errors
 set -e
 
 # build
-npm run build
+pnpm run build
 
 # navigate into the build output directory
 cd dist
@@ -13,12 +13,12 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout -b release
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<rainbowflesh>.github.io
-git push -f git@github.com: main <rainbowflesh >/ <rainbowflesh >.github.io.git
+git push -f git@github.com:rainbowflesh/rainbowflesh.github.io.git
 
 # if you are deploying to https://<rainbowflesh>.github.io/<REPO>
 # git push -f git@github.com:<rainbowflesh>/<REPO>.git main:gh-pages
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # rainbowflesh.github.io
+
 Blog page resource repo.
 
 ## WIP
 
 ## TODO
-TODO: imitate https://musicforprogramming.net/latest/
+
+TODO: imitate <https://musicforprogramming.net/latest/>
+TODO: vite build
+TODO: blog add kismet deploy
+TODO: blog add blog creation
+TODO: blog add vite build bug
