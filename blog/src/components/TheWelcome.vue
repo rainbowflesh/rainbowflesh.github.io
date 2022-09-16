3063ca15bbfc715673fb3035eccfed1cd7168e94
rainbowflesh <rainbow@qiuqi-tech.com>
2022-09-16 19:30:26 +0800

    test(add test): test rainbow css

--- blog/src/components/TheWelcome.vue
+++ blog/src/components/TheWelcome.vue
@@ -3,7 +3,29 @@
 <template>
   <div class="container">
     <div class="header">
-      <p class="heading">Rainbow's Blog</p>
+      <div class="heading">
+        <ul class="c-rainbow" style="z-index: 1">
+          <li class="c-rainbow__layer c-rainbow__layer--white">
+            RAINBOW`s Blog
+          </li>
+          <li class="c-rainbow__layer c-rainbow__layer--orange">
+            RAINBOW`s Blog
+          </li>
+          <li class="c-rainbow__layer c-rainbow__layer--red">RAINBOW`s Blog</li>
+          <li class="c-rainbow__layer c-rainbow__layer--violet">
+            RAINBOW`s Blog
+          </li>
+          <li class="c-rainbow__layer c-rainbow__layer--blue">
+            RAINBOW`s Blog
+          </li>
+          <li class="c-rainbow__layer c-rainbow__layer--green">
+            RAINBOW`s Blog
+          </li>
+          <li class="c-rainbow__layer c-rainbow__layer--yellow">
+            RAINBOW`s Blog
+          </li>
+        </ul>
+      </div>
       <p
         class="desc"
         style="text-align: center; font-family: Geneva_9; font-size: 2em"
@@ -60,5 +82,102 @@ blockquote {
 blockquote footer {
   margin: 12px 0 0 12px;
   font-size: 11px;
+}
+</style>
+<style lang="scss">
+$root: ".c-rainbow";
+
+:root {
+  --color-background: #31037d;
+  --axis-x: 1px;
+  --axis-y: 0.5rem;
+  --delay: 10;
+
+  --color-black: #000;
+  --color-white: #fff;
+  --color-orange: #d49c3d;
+  --color-red: #d14b3d;
+  --color-violet: #cf52eb;
+  --color-blue: #44a3f7;
+  --color-green: #5acb3c;
+  --color-yellow: #debf40;
+
+  --color-foreground: var(--color-white);
+  --font-name: Righteous;
+}
+
+#{$root} {
+  counter-reset: rainbow;
+  position: relative;
+  display: flex;
+  align-items: center;
+  list-style: none;
+
+  padding: 0;
+  margin: 0;
+  top: 0.4rem;
+  text-align: center;
+  margin: 0 auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  &__layer {
+    --text-color: var(--color-foreground);
+    counter-increment: rainbow;
+
+    font-size: 3rem;
+    color: var(--text-color);
+
+    text-shadow: -1px -1px 0 var(--color-black), 1px -1px 0 var(--color-black),
+      -1px 1px 0 var(--color-black), 1px 1px 0 var(--color-black),
+      4px 4px 0 rgba(0, 0, 0, 0.2);
+
+    animation: rainbow 1.5s ease-in-out infinite;
+
+    @for $i from 1 through 7 {
+      &:nth-child(#{$i}) {
+        animation-delay: calc(#{$i} / var(--delay) * 1s);
+        left: calc(var(--axis-x) * #{$i});
+        z-index: -#{$i * 10};
+      }
+    }
+
+    &:not(:first-child) {
+      position: absolute;
+      top: 0;
+    }
+
+    &--white {
+      --text-color: var(--color-white);
+    }
+    &--orange {
+      --text-color: var(--color-orange);
+    }
+    &--red {
+      --text-color: var(--color-red);
+    }
+    &--violet {
+      --text-color: var(--color-violet);
+    }
+    &--blue {
+      --text-color: var(--color-blue);
+    }
+    &--green {
+      --text-color: var(--color-green);
+    }
+    &--yellow {
+      --text-color: var(--color-yellow);
+    }
+  }
+}
+
+@keyframes rainbow {
+  0%,
+  100% {
+    transform: translatey(var(--axis-y));
+  }
+  50% {
+    transform: translatey(calc(var(--axis-y) * -1));
+  }
 }
 </style>
